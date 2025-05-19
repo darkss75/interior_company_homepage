ecd77aec1ca293713d9ae8eb183632aa7aa87adb
darkss75 <54921035+darkss75@users.noreply.github.com>
2025-05-19 11:13:22 +0900

    GitHub Pages basePath, assetPrefix 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/interior_company_homepage',
+  assetPrefix: '/interior_company_homepage/',
   eslint: {
     ignoreDuringBuilds: true,
   },
